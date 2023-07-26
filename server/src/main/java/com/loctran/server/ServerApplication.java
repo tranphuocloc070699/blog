3117b3c0f6d79e8c6ad12eed0c2f0742ffd2d737
Trần Phước Lộc <tranphuocloc070699@gmail.com>
2023-07-26 08:27:43 +0700

    office

--- server/src/main/java/com/loctran/server/ServerApplication.java
+++ server/src/main/java/com/loctran/server/ServerApplication.java
@@ -7,8 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServerApplication {
 
 	public static void main(String[] args) {
-
+	
 		SpringApplication.run(ServerApplication.class, args);
+		
+		
 	}
 
 }
