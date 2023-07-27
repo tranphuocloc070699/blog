f3431ec0e54dbac2b2209baa465926e6822eb702
Trần Phước Lộc <tranphuocloc070699@gmail.com>
2023-07-27 16:46:14 +0700

    test message 7

--- server/src/main/java/com/loctran/server/ServerApplication.java
+++ server/src/main/java/com/loctran/server/ServerApplication.java
@@ -9,7 +9,7 @@ public class ServerApplication {
 	public static void main(String[] args) {
 	
 		SpringApplication.run(ServerApplication.class, args);
-		System.out.println("1123123dadasdasdaddsadsadasdada");
+	
 	}
 
 }
